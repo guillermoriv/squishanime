ead492d972704ec210285474ab8e0a20e23ece6e
Guillermo Rivas <guillermo.campanudo@hotmail.com>
2022-01-28 10:50:48 +0000

    Updating the days in the weekly update

--- src/components/CardDay/CardDay.tsx
+++ src/components/CardDay/CardDay.tsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { weekSpanish } from '../../utils/constants';
 
 export const CardDay: React.FC<{ day: string }> = ({ day }: { day: string }) => {
   const [schedule, setSchedule] = useState<any[]>([]);
@@ -18,30 +20,29 @@ export const CardDay: React.FC<{ day: string }> = ({ day }: { day: string }) => 
     <div>
       <h6 className="text-base font-medium leading-tight p-2 text-gray-800 cursor-pointer" onClick={handleClick}>
         <span className="inline-block py-1.5 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-blue-600 text-white rounded">
-          {day}
+          {weekSpanish[day]}
         </span>
       </h6>
       {schedule.length > 0 ? (
         <div className="grid grid-cols-1 xl:grid-cols-3 border-2 rounded-md p-2" id={day}>
           {schedule.map((item, index) => {
             return (
-              <div className="flex justify-center p-2" key={index}>
-                <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
-                  <img
-                    className="w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
-                    src={item.images.jpg.image_url}
-                    alt=""
-                  />
-                  <div className="p-6 flex flex-col justify-start">
-                    <h5 className="text-gray-900 text-xl font-medium mb-2">{item.title}</h5>
-                    <p className="text-gray-700 text-base mb-4">
-                      This is a wider card with supporting text below as a natural lead-in to additional content. This
-                      content is a little bit longer.
-                    </p>
-                    <p className="text-gray-600 text-xs">Last updated 3 mins ago</p>
+              <Link to={`../anime/${item.id}`} className="cursor-pointer" key={index}>
+                <div className="flex justify-center p-2">
+                  <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+                    <img
+                      className="w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                      src={item.images.jpg.image_url}
+                      alt=""
+                    />
+                    <div className="p-6 flex flex-col justify-start">
+                      <h5 className="text-gray-900 text-xl font-medium mb-2">{item.title}</h5>
+                      <p className="text-gray-700 text-base mb-4">This is just for testing porpuses.</p>
+                      <p className="text-gray-600 text-xs">Last updated 3 mins ago</p>
+                    </div>
                   </div>
                 </div>
-              </div>
+              </Link>
             );
           })}
         </div>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,10 @@
 export const daysOfWeek: string[] = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+export const weekSpanish: { [key: string]: string } = {
+  monday: 'Lunes',
+  tuesday: 'Martes',
+  wednesday: 'Miercoles',
+  thursday: 'Jueves',
+  friday: 'Viernes',
+  saturday: 'Sábado',
+  sunday: 'Domingo',
+};
